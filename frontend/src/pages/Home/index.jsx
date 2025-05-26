ebb1b916970d70dba43366b3af36e59b5bf542b1
Tokosama <anouartoko@gmail.com>
2025-05-26 23:44:09 +0100

    DEbut du service dauth

---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Index() {
+  return (
+    <div>
+        <div className='bg-red-500 flex justify-center   w-full h-full'>
+            Welcome
+            <button className="btn">Default</button>
+        </div>
+    </div>
+  )
+}
