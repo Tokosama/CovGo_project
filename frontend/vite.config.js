b386d134fc8b305b3442de402e51ac51faaa1de0
Tokosama <anouartoko@gmail.com>
2025-06-13 01:55:11 +0100

    Frontend: modifications du vite.config pour demarrer l'app sur toutes les platforme

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    server: {
+    host: '0.0.0.0', // ← c'est ESSENTIEL
+    port: 5173       // ← ou le port que tu veux
+  }
 });
