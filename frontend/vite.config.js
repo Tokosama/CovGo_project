28608a05c03e9621ebc3cfa2a151971722d177ee
Tokosama <anouartoko@gmail.com>
2025-06-17 13:45:41 +0100

    Commit before merge

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,25 +5,25 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    server: {
-    host: '0.0.0.0', // ← c'est ESSENTIEL
-    port: 5173       // ← ou le port que tu veux
-  }
+  server: {
+    host: "0.0.0.0", // ← c'est ESSENTIEL
+    port: 5173, // ← ou le port que tu veux
+  },
   build: {
-    minify: 'terser',
+    minify: "terser",
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          ui: ['@fortawesome/react-fontawesome', 'framer-motion']
-        }
-      }
-    }
+          vendor: ["react", "react-dom", "react-router-dom"],
+          ui: ["@fortawesome/react-fontawesome", "framer-motion"],
+        },
+      },
+    },
   },
-  server: {
-    hmr: true,
-    watch: {
-      usePolling: true
-    }
-  }
+  // server: {
+  //   hmr: true,
+  //   watch: {
+  //     usePolling: true
+  //   }
+  // }
 });
